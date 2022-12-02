fc78095c2a2b00c298382fae9a803a7efa4cb7ea
Alexis Aldrete Git <alexis.aldrete96@gmail.com>
2022-12-01 19:21:03 -0800

    [Issue #3 ][FE] Fixing syntax

--- app/real-estate-app/src/app/app.module.ts
+++ app/real-estate-app/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
